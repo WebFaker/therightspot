31bf31821160c10e8bc320026cfc8500ce828b90
ShAd0wInK <alexandredelaloy@live.com>
2018-07-06 21:55:41 +0200

    :zap: add new gulp task who copy assets & html

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,17 @@ const rename = require('gulp-rename');
 
 // Build tasks ----------------------------------
 
+// ==> HTML -------------------------------------
+    // when I write 'gulp html' in the terminal
+gulp.task('html', function() {
+    // Take the html file
+    return gulp.src('src/index.html')
+    // put the file in ./public
+    .pipe(gulp.dest('public'))
+});
+
 // ==> SCSS -------------------------------------
-// when I write 'gulp scss' in the terminal
+    // when I write 'gulp scss' in the terminal
 gulp.task('scss', function() {
     // Take the main scss file
     return gulp.src('src/scss/master.scss')
@@ -27,7 +36,7 @@ gulp.task('scss', function() {
 });
 
 // ==> Scripts ----------------------------------
-// when I write 'gulp scripts' in the terminal
+    // when I write 'gulp scripts' in the terminal
 gulp.task('js', function() {
     // take the main js file
     return gulp.src('src/js/app.js')
@@ -47,13 +56,24 @@ gulp.task('js', function() {
     .pipe(browserSync.stream());
 });
 
+// ==> Assets -----------------------------------
+    // when I write 'gulp assets' in the terminal
+gulp.task('assets', function() {
+    // Take the assets folder
+    return gulp.src('src/assets/**/*')
+    // put the file in ./public
+    .pipe(gulp.dest('public/assets'))
+});
+
 // ==> Build files ------------------------------
-gulp.task('build', ['scss', 'js']);
+gulp.task('build', ['html','scss','js','assets']);
 
 // ==> Watch files ------------------------------
 gulp.task('watch', function() {
+    gulp.watch('src/**/*.html', ['html']);
     gulp.watch('src/js/**/*.js', ['js']);
     gulp.watch('src/scss/**/*.scss', ['scss']);
+    gulp.watch('src/assets', ['assets']);
 });
 
 // ==> browserSync ------------------------------
