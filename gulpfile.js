55e3996cb33ba6ad931d7d325935a65d92d6c207
ShAd0wInK <alexandredelaloy@live.com>
2018-07-06 18:21:48 +0200

    🍱 Update assets

--- gulpfile.js
+++ gulpfile.js
@@ -10,36 +10,49 @@ const rename = require('gulp-rename');
 // Build tasks ----------------------------------
 
 // ==> SCSS -------------------------------------
+// when I write 'gulp scss' in the terminal
 gulp.task('scss', function() {
+    // Take the main scss file
     return gulp.src('src/scss/master.scss')
+    // minify the file
     .pipe(sass({
         outputStyle: 'compressed'
     }).on('error', sass.logError))
+    // rename the minified file
     .pipe(rename('master.min.css'))
+    // put the file in ./public/css
     .pipe(gulp.dest('public/css'))
+    // Update the modification in the hist with BS
     .pipe(browserSync.stream());
 });
 
 // ==> Scripts ----------------------------------
-gulp.task('scripts', function() {
+// when I write 'gulp scripts' in the terminal
+gulp.task('js', function() {
+    // take the main js file
     return gulp.src('src/js/app.js')
+    // ??
     .pipe(babel())
+    // ??
     .pipe(webpack({
         output: {
             filename: 'app.min.js'
         }
     }))
+    // uglify the file ( rename var, etc )
     .pipe(uglify())
+    // put the file in ./public/js
     .pipe(gulp.dest('public/js'))
+    // Update the modification in the hist with BS
     .pipe(browserSync.stream());
 });
 
 // ==> Build files ------------------------------
-gulp.task('build', ['scss', 'scripts']);
+gulp.task('build', ['scss', 'js']);
 
 // ==> Watch files ------------------------------
 gulp.task('watch', function() {
-    gulp.watch('src/js/**/*.js', ['scripts']);
+    gulp.watch('src/js/**/*.js', ['js']);
     gulp.watch('src/scss/**/*.scss', ['scss']);
 });
 
---
+++ src/js/ui.js
@@ -0,0 +1,9 @@
+window.addEventListener('keydown', function(event) {
+    if ( event.code === 'Space' && event.code === 'Enter' ) {
+        
+        console.log('🎉')
+    }
+    else if (event.code === 'MetaLeft' ) {
+        console.log('🎉🎉')
+    }
+})
