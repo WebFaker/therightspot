1e63373bc75d4c7b2c992786eb29bd24cfbfcace
ShAd0wInK <alexandredelaloydev@gmail.com>
2018-10-18 15:34:26 +0200

    add html gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const cleancss = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
 const plumber = require('gulp-plumber');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify-es').default;
+
+gulp.task('html', () => {
+	return gulp.src('src/*.html')
+		.pipe(htmlmin({ collapseWhitespace: true }))
+		.pipe(gulp.dest('public'));
+});
 
 gulp.task('scss', () => {
 	return gulp.src('src/scss/master.scss')
