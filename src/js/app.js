081315b393696049fa3dac3a4d1793346caeb0e2
ShAd0wInK <alexandredelaloydev@gmail.com>
2018-10-18 17:49:55 +0200

    :fire: renaming file and add a little ui

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,5 @@
-// const myFn = (x, y) => { return x * y; };
+// ==============================================
+// IMPORT JS FILES
+// ==============================================
 
-// console.log(myFn(3, 9));
-
-// const myArray = [2, 5, 9].map(x => x ** 2).join(' - ');
-
-// console.log(myArray);
-
-"use strict";
-
-var myFn = function myFn(x, y) {
-  return x * y;
-};
-
-console.log(myFn(3, 9));
-var myArray = [2, 5, 9].map(function (x) {
-  return Math.pow(x, 2);
-}).join(' - ');
-console.log(myArray);
+import './ui';
---
+++ src/js/ui.js
@@ -0,0 +1,7 @@
+const myFn = (x, y) => { return x * y; };
+
+console.log(myFn(3, 9));
+
+const myArray = [2, 5, 9].map(x => x ** 2).join(' - ');
+
+console.log(myArray);
