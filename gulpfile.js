657170117596b630cf8151d15148ab112e2653b6
ShAd0wInK <alexandredelaloydev@gmail.com>
2018-10-18 12:20:25 +0200

    :wrench: add scss gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const cleancss = require('gulp-clean-css');
+const plumber = require('gulp-plumber');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('scss', () => {
+	return gulp.src('src/scss/master.scss')
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(cleancss({ compatibility: 'ie8' }))
+		.pipe(rename('master.min.css'))
+		.pipe(gulp.dest('public/css'));
+});
 
 gulp.task('js', () => {
 	return gulp.src('src/js/app.js')
