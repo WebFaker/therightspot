071eae4626871862cd8e7c296eb5504056ce530e
ShAd0wInK <alexandredelaloydev@gmail.com>
2018-10-18 16:26:21 +0200

    :wrench: add build, watch & default tasks, and browsersync to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const browserSync = require('browser-sync').create();
 const cleancss = require('gulp-clean-css');
 const htmlmin = require('gulp-htmlmin');
 const plumber = require('gulp-plumber');
@@ -11,7 +12,8 @@ const uglify = require('gulp-uglify-es').default;
 gulp.task('html', () => {
 	return gulp.src('src/*.html')
 		.pipe(htmlmin({ collapseWhitespace: true }))
-		.pipe(gulp.dest('public'));
+		.pipe(gulp.dest('public'))
+		.pipe(browserSync.stream());
 });
 
 gulp.task('scss', () => {
@@ -21,21 +23,40 @@ gulp.task('scss', () => {
 		.pipe(sass().on('error', sass.logError))
 		.pipe(cleancss({ compatibility: 'ie8' }))
 		.pipe(rename('master.min.css'))
-		.pipe(gulp.dest('public/css'));
+		.pipe(gulp.dest('public/css'))
+		.pipe(browserSync.stream());
 });
 
 gulp.task('js', () => {
 	return gulp.src('src/js/app.js')
 		.pipe(plumber())
+		.pipe(sourcemaps.init())
 		.pipe(babel())
 		.pipe(rename('app.min.js'))
-		.pipe(sourcemaps.init())
 		.pipe(uglify())
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('public/js'));
+		.pipe(gulp.dest('public/js'))
+		.pipe(browserSync.stream());
 });
 
 gulp.task('assets', () => {
 	return gulp.src('src/assets/**/*')
-		.pipe(gulp.dest('public/assets'));
-})
+		.pipe(gulp.dest('public/assets'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('build', ['html', 'scss', 'js', 'assets']);
+
+gulp.task('watch', () => {
+	gulp.watch('src/**/*.html', ['html']);
+	gulp.watch('src/js/**/*.js', ['js']);
+	gulp.watch('src/scss/**/*.scss', ['scss']);
+	gulp.watch('src/assets', ['assets']);
+});
+
+gulp.task('default', ['watch'], () => {
+	browserSync.init({
+		server: {
+			baseDir: './public',
+		},
+	});
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,19 @@
-const myFn = (x, y) => { return x * y; };
+// const myFn = (x, y) => { return x * y; };
+
+// console.log(myFn(3, 9));
+
+// const myArray = [2, 5, 9].map(x => x ** 2).join(' - ');
+
+// console.log(myArray);
+
+"use strict";
+
+var myFn = function myFn(x, y) {
+  return x * y;
+};
 
 console.log(myFn(3, 9));
-
-const myArray = [2, 5, 9].map(x => x ** 2).join(' - ');
-
+var myArray = [2, 5, 9].map(function (x) {
+  return Math.pow(x, 2);
+}).join(' - ');
 console.log(myArray);
